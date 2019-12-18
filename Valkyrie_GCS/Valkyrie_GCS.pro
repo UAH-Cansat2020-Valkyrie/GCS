c58f3ca7c2a05e4bcc8c43fb878d17283d0aeddf
Nathan Ulmer <NathanFillion@users.noreply.github.com>
2019-12-18 10:01:31 -0600

    Started layout
    Look in the google drive for a brief outline of the  elemtns to be included

--- Valkyrie_GCS/Valkyrie_GCS.pro
+++ Valkyrie_GCS/Valkyrie_GCS.pro
@@ -1,4 +1,4 @@
-QT       += core gui serialport multimedia
+QT += core gui serialport multimedia
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 
--- build-Valkyrie_GCS-Desktop_Qt_5_12_4_MinGW_32_bit-Debug/ui_mainwindow.h
+++ build-Valkyrie_GCS-Desktop_Qt_5_12_4_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -10,8 +10,11 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
@@ -21,24 +24,79 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *actionPort;
+    QAction *actionBaud_Rate;
+    QAction *actionStop_Bits;
+    QAction *actionAbout;
+    QAction *actionCredits;
+    QAction *actionSave_As;
+    QAction *actionSave;
+    QAction *actionExit;
     QWidget *centralwidget;
+    QGridLayout *gridLayout;
     QMenuBar *menubar;
+    QMenu *menuFile;
+    QMenu *menuView;
+    QMenu *menuSerial_Settings;
+    QMenu *menuHelp;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(1270, 720);
+        actionPort = new QAction(MainWindow);
+        actionPort->setObjectName(QString::fromUtf8("actionPort"));
+        actionBaud_Rate = new QAction(MainWindow);
+        actionBaud_Rate->setObjectName(QString::fromUtf8("actionBaud_Rate"));
+        actionStop_Bits = new QAction(MainWindow);
+        actionStop_Bits->setObjectName(QString::fromUtf8("actionStop_Bits"));
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QString::fromUtf8("actionAbout"));
+        actionCredits = new QAction(MainWindow);
+        actionCredits->setObjectName(QString::fromUtf8("actionCredits"));
+        actionSave_As = new QAction(MainWindow);
+        actionSave_As->setObjectName(QString::fromUtf8("actionSave_As"));
+        actionSave = new QAction(MainWindow);
+        actionSave->setObjectName(QString::fromUtf8("actionSave"));
+        actionExit = new QAction(MainWindow);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 1270, 21));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        menuView = new QMenu(menubar);
+        menuView->setObjectName(QString::fromUtf8("menuView"));
+        menuSerial_Settings = new QMenu(menubar);
+        menuSerial_Settings->setObjectName(QString::fromUtf8("menuSerial_Settings"));
+        menuHelp = new QMenu(menubar);
+        menuHelp->setObjectName(QString::fromUtf8("menuHelp"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menubar->addAction(menuView->menuAction());
+        menubar->addAction(menuSerial_Settings->menuAction());
+        menubar->addAction(menuHelp->menuAction());
+        menuFile->addAction(actionSave_As);
+        menuFile->addAction(actionSave);
+        menuFile->addSeparator();
+        menuFile->addAction(actionExit);
+        menuSerial_Settings->addAction(actionPort);
+        menuSerial_Settings->addAction(actionBaud_Rate);
+        menuSerial_Settings->addAction(actionStop_Bits);
+        menuHelp->addSeparator();
+        menuHelp->addAction(actionAbout);
+        menuHelp->addAction(actionCredits);
 
         retranslateUi(MainWindow);
 
@@ -48,6 +106,28 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionPort->setText(QApplication::translate("MainWindow", "Port", nullptr));
+        actionBaud_Rate->setText(QApplication::translate("MainWindow", "Baud Rate", nullptr));
+        actionStop_Bits->setText(QApplication::translate("MainWindow", "Stop Bits", nullptr));
+        actionAbout->setText(QApplication::translate("MainWindow", "About", nullptr));
+        actionCredits->setText(QApplication::translate("MainWindow", "Credits", nullptr));
+        actionSave_As->setText(QApplication::translate("MainWindow", "Save As              ctrl+shift+s", nullptr));
+        actionSave_As->setIconText(QApplication::translate("MainWindow", "Save As              ctrl+shift+s", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionSave_As->setToolTip(QApplication::translate("MainWindow", " (ctrl+shift+s)", nullptr));
+#endif // QT_NO_TOOLTIP
+        actionSave->setText(QApplication::translate("MainWindow", "Save", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionSave->setToolTip(QApplication::translate("MainWindow", "Save (ctrl+s)", nullptr));
+#endif // QT_NO_TOOLTIP
+        actionExit->setText(QApplication::translate("MainWindow", "Exit", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionExit->setToolTip(QApplication::translate("MainWindow", "Exit (Ctrl+Q)", nullptr));
+#endif // QT_NO_TOOLTIP
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", nullptr));
+        menuView->setTitle(QApplication::translate("MainWindow", "View", nullptr));
+        menuSerial_Settings->setTitle(QApplication::translate("MainWindow", "Serial Settings", nullptr));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "Help", nullptr));
     } // retranslateUi
 
 };
